044ed5b8ae4061014d78e32fcc22c7397e18f56e
andreazevedo80 <andre.l.azevedo69@gmail.com>
2024-11-21 10:44:33 -0300

    subindo cenário 02 com os testes funcionando

--- cypress/e2e/Serverest/02-login.cy.js
+++ cypress/e2e/Serverest/02-login.cy.js
@@ -1,66 +1,90 @@
+// Importar o Faker.js
+import { faker } from '@faker-js/faker';
+
 describe('Cenário: Verificar a funcionalidade do Login', () => {
+    
+    let nome;
+    let email;
+    let senha;
+
+    before(() => {
+       nome = faker.internet.username({firstName: 'Avanti'});
+       email = faker.internet.email({firstName: 'Avanti', provider: 'squad01.avanti.dev'});
+       senha = faker.internet.password({length: 10, memorable:true, pattern: /[A-Z][0-9]/, prefix: 'Avanti@'});
+    
+       cy.visit('https://front.serverest.dev/cadastrarusuarios');
+       cy.get('input[id="nome"]').type(nome);
+       cy.get('input[id="email"]').type(email);
+       cy.get('input[id="password"]').type(senha);
+       cy.get('button[data-testid="cadastrar"]').click();
+       cy.wait(3000);
+       cy.url().should('eq', 'https://front.serverest.dev/home');
+       cy.get('button[data-testid="logout"]').click();
+    })
+    beforeEach(() => {
+       cy.wait(2000);
+       cy.visit('https://front.serverest.dev/login');
+       cy.wait(2000);
+    })
+
     it('CT02-001 Login com credenciais corretas', () => {
-        //Pré Condições
-        cy.visit('https://front.serverest.dev/login');
 
        //Dados de entrada & Passo a Passo
-        cy.get('input[id="email"]').type('avanti01@outlook.com');
-       cy.get('input[id="password"]').type('Avanti@123');
-       cy.get('button[id="cadastrar"]').click();
+       cy.get('input[id="email"]').type(email);
+       cy.get('input[id="password"]').type(senha);
+       cy.get('button[data-testid="entrar"]').click();
 
        //Resultado Esperado: O usuário deve logar e ser direcionado para Home Page
-       cy.url().should('eq', 'https://front.serverest.dev/home');      
+       cy.wait(4000);
+       cy.url().should('eq', 'https://front.serverest.dev/home');    
     });
- //   it('CT02-002 Login com credenciais incorretas', () => {
- //       //Pré Condições
- //       cy.visit('https://front.serverest.dev/login');
+
+    it('CT02-002 Login com credenciais incorretas', () => {
+       const senhainvalida = 'senhainvalida';
 
        //Dados de entrada & Passo a Passo
-//       cy.get('input[id="email"]').type('avanti01@outlook.com');
-//       cy.get('input[id="password"]').type('Avanti1234');
-//       cy.get('button[data-testeid="cadastrar"]').click();
+       cy.get('input[id="email"]').type(email);
+       cy.get('input[id="password"]').type(senhainvalida);
+       cy.get('button[data-testid="entrar"]').click();
 
        //Resultado Esperado: O sistema deve exibir uma mensagem indicando “Email e/ou senha inválidos”
-//       cy.url().should('eq', 'https://front.serverest.dev/home');      
-//    });
+       cy.wait(3000);
+       cy.get('.alert').contains('Email e/ou senha inválidos').should('be.visible');
+    });
 
-//   it('CT02-004 Login com campo de e-mail vazio', () => {
- //       //Pré Condições
- //       cy.visit('https://front.serverest.dev/login');
+   it('CT02-004 Login com campo de e-mail vazio', () => {
 
        //Dados de entrada & Passo a Passo
-//       cy.get('input[id="email"]').type('');
-//       cy.get('input[id="password"]').type('Teste@123');
-//       cy.get('button[data-testeid="cadastrar"]').click();
+       cy.get('input[id="password"]').type(senha);
+       cy.get('button[data-testid="entrar"]').click();
 
        //Resultado Esperado: O sistema deve exibir mensagem indicando “Email é obrigatório”
-//       cy.url().should('eq', 'https://front.serverest.dev/home');      
-//    });
+       cy.wait(3000);
+       cy.get('.alert').contains('Email é obrigatório').should('be.visible');  
+    });
 
-//   it('CT02-005 Login com campo de senha vazio', () => {
- //       //Pré Condições
- //       cy.visit('https://front.serverest.dev/login');
+   it('CT02-005 Login com campo de senha vazio', () => {
 
        //Dados de entrada & Passo a Passo
-//       cy.get('input[id="email"]').type('avanti01@outlook.com');
-//       cy.get('input[id="password"]').type('');
-//       cy.get('button[data-testeid="cadastrar"]').click();
+       cy.get('input[id="email"]').type(email);
+       cy.get('button[data-testid="entrar"]').click();
 
        //Resultado Esperado: O sistema deve exibir mensagem indicando “Password é obrigatório”
-//       cy.url().should('eq', 'https://front.serverest.dev/home');      
-//    });
+       cy.wait(3000);
+       cy.get('.alert').contains('Password é obrigatório').should('be.visible');   
+    });
 
-//   it('CT02-006 Login com e-mail inválido', () => {
- //       //Pré Condições
- //       cy.visit('https://front.serverest.dev/login');
+   it('CT02-006 Login com e-mail inválido', () => {
+       const emailInvalido = 'emailinvalido@emailinvalido.com.br';
 
        //Dados de entrada & Passo a Passo
-//       cy.get('input[id="email"]').type('teste@lol.com.br');
-//       cy.get('input[id="password"]').type('Avanti@123');
-//       cy.get('button[data-testeid="cadastrar"]').click();
+       cy.get('input[id="email"]').type(emailInvalido);
+       cy.get('input[id="password"]').type(senha);
+       cy.get('button[data-testid="entrar"]').click();
 
        //Resultado Esperado: O sistema deve exibir mensagem indicando “Email e/ou senha inválidos”
-//       cy.url().should('eq', 'https://front.serverest.dev/home');      
-//    });
+       cy.wait(3000);
+       cy.get('.alert').contains('Email e/ou senha inválidos').should('be.visible');     
+    });
 
 });
